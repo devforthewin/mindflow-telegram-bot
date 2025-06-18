5ed8b4c919e2c762a5090635cfc8fa615ed4ea7c
arbuz.tp <arbuz.tp@gmail.com>
2025-06-18 15:15:33 +0300

    refactor: update client and server configuration and bootstrap

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  await app.listen(3000);
+  
+  process.on('SIGINT', async () => {
+    console.log('SIGINT received');
+    await app.close();
+    process.exit(0);
+  });
+  
+  process.on('SIGTERM', async () => {
+    console.log('SIGTERM received');
+    await app.close();
+    process.exit(0);
+  });
 }
 bootstrap();
